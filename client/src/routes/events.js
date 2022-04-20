760c2f9c3cd6bbfd4eef6e2d499033e15fc1e1d1
Gopinath Gangisetti <ggangisetti@unomaha.edu>
2022-04-20 17:03:35 -0500

    Initial commit for events functionality

--- client/src/routes/events.js
+++ client/src/routes/events.js
@@ -1,15 +1,33 @@
-import React, { memo } from 'react'
+import * as React from "react";
+import ErrorComponent from "../Components/ErrorComponent";
 
-import PropTypes from 'prop-types'
+import Event from "../Components/Event";
+import Pagination from "../Components/Pagination";
+import { EventsURL } from "../Constants/AppLinks";
 
-function Events({}) {
+function Events() {
+  const [events, setEvents] = React.useState(null);
+  const [error, setError] = React.useState(null);
+
+  const fetchPageData = (pageNumber) => {
+    fetch(`${EventsURL}/?page=${pageNumber}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setEvents(res.events);
+      })
+      .catch((err) => {
+        setError("failed to fetch data!");
+      });
+  };
+
+  if(error) return <ErrorComponent error={error} />
+
   return (
-    <div>Events List</div>
-  )
+    <div>
+      <Event />
+      <Pagination fetchPageData={fetchPageData} />
+    </div>
+  );
 }
 
-Events.propTypes = {
-   
-}
-
-export default memo(Events);
+export default React.memo(Events);
