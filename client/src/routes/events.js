6926f575c009ed4d2e0ceeb7774152930e4ca8f6
Gopinath Gangisetti <ggangisetti@unomaha.edu>
2022-05-09 09:51:34 -0500

    Added Event Registration

--- client/src/routes/events.js
+++ client/src/routes/events.js
@@ -3,14 +3,18 @@ import ErrorComponent from "../Components/ErrorComponent";
 import DateTimePicker from 'react-datetime-picker'
 import Event from "../Components/Event";
 import { EventsURL } from "../Constants/AppLinks";
+import { useLocation } from "react-router-dom";
 
 function Events() {
   const [events, setEvents] = React.useState(null);
   const [error, setError] = React.useState(null);
   const [date, setDate] = React.useState(() => new Date());
+  const { state } = useLocation();
+  const { userId} = state || {};
 
-  const fetchPageData = (filters) => {
-    fetch(`${EventsURL}`)
+  const fetchPageData = (userId) => {
+    const url = userId ? `${EventsURL}/user/${userId}`: EventsURL
+    fetch(url)
       .then((res) => res.json())
       .then((res) => {
         setEvents(res.events);
@@ -21,8 +25,8 @@ function Events() {
   };
 
   React.useEffect(() => {
-    fetchPageData(date);
-  }, [date]);
+    fetchPageData(userId);
+  }, [userId]);
 
   if (error) return <ErrorComponent error={error} />;
 
@@ -32,11 +36,13 @@ function Events() {
   return (
     <div className="container h-100">
       <div className="m-3">
-       <button className="m-2 p-1 rounded today-btn" >Today</button>
-        <DateTimePicker onChange={setDate} value={date} disableClock={true} />
+       {/* <button className="m-2 p-1 rounded today-btn" name="today-btn">Today</button>
+        <DateTimePicker onChange={setDate} value={date} disableClock={true} /> */}
+        <h4 className="primary-text">{userId ? 'My Events' : 'Events'}</h4>
       </div>
+      <hr />
       {events.map((event) => (
-        <Event event={event} />
+        <Event event={event} key={event.Event_Id}/>
       ))}
     </div>
   );
